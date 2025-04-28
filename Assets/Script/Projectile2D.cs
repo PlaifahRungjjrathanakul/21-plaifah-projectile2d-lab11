41f6c328c5fb8d6ae690f1ccb852c286eec91c63
PlaifahRungjjrathanakul <plaifah.rung@bumail.net>
2025-04-28 15:11:11 +0700

    Update

--- Assets/Script/Projectile2D.cs
+++ Assets/Script/Projectile2D.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Projectile2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject target; // target sprite
+    [SerializeField] Rigidbody2D bulletPrefab;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            // shoot raycast to detect mouse clicked position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+
+            // get click point
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            // if hit object with collider
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("hit " + hit.collider.name);
+            }
+        }
     }
 }
